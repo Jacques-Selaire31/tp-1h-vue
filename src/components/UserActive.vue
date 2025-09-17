f1fcd0075355164f07010b4411b59ab525b0624e
Jacques-Selaire31 <serra.victor@yahoo.fr>
2025-09-17 12:23:33 +0200

    TP eval 1h

---
+++ src/components/UserActive.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="display-user">
+
+    <h1>Composant affichage des données utilisateurs</h1>
+    <p v-for="user in users" :name="user.name" :age="user.age"></p>
+  </div>
+  <UserData @add-user="DisplayUser" />
+</template>
+
+<script setup lang='js'>
+import { computed, watch, onMounted, onUpdated, onBeforeUnmount, ref } from 'vue'
+import UserData from './UserData.vue';
+
+
+const users = ref([{ name: "John", age: 25 }]);
+
+const DisplayUser = () => {
+  users.value.push({ name: name.value, age: age.value });
+  console.log(users);
+}
+
+
+</script>
+<style scoped lang="css"></style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,30 @@
+<template>
+    <label for="name">Nom:</label>
+    <input v-model="name" type="text" placeholder="Entrer un nom" class="input input-primary" />
+    <label for="age">Age:</label>
+    <input v-model="age" type="number" placeholder="Entrer votre age" class="input input-primary" />
+    <button @click="pushUserToDisplay" class="btn btn-secondary">Mettre à jour</button>
+</template>
+<script setup>
+import { ref } from 'vue';
+const name = ref("");
+const age = ref();
+console.log(name, age);
+const emit = defineEmits("add-user");
+const pushUserToDisplay = () => {
+    emit("add-user", name.value, age.value);
+    console.log(name.value, age.value);
+}
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+})
+
+</script>
+<style scoped></style>
